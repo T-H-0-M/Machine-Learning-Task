16e7ebd53814f0a5e15dad4bf7aa9067a1420d2c
Thomas <thomasbandy23@gmail.com>
2023-04-05 21:56:08 +1000

    Added functions

--- nn_training.py
+++ nn_training.py
@@ -3,12 +3,14 @@
 # Description:
 
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import torch
+import pandas
 
 from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
 from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix, classification_report
 
 
 class Train():
@@ -33,51 +35,82 @@ class Train():
         plt.show()
 
     def solve(self, num_epochs, num_folds):
-        train_losses = []
-        train_accuracies = []
-        test_losses = []
-        test_accuracies = []
+        self.train_losses = []
+        self.train_accuracies = []
+        self.test_losses = []
+        self.test_accuracies = []
 
         kfold = KFold(
             n_splits=num_folds, random_state=17, shuffle=True)
 
         for i, (train_index, test_index) in enumerate(kfold.split(self.x)):
             print(f"Fold {i}")
-            x_train, x_test = self.x[train_index], self.x[test_index]
-            y_train, y_test = self.y[train_index], self.y[test_index]
+            self.x_train, self.x_test = self.x[train_index], self.x[test_index]
+            self.y_train, self.y_test = self.y[train_index], self.y[test_index]
 
             # Convert to tensors
-            x_train = torch.from_numpy(x_train).type(torch.float32)
-            x_test = torch.from_numpy(x_test).type(torch.float32)
-            y_train = torch.from_numpy(y_train)
-            y_test = torch.from_numpy(y_test)
+            self.x_train = torch.from_numpy(self.x_train).type(torch.float32)
+            self.x_test = torch.from_numpy(self.x_test).type(torch.float32)
+            self.y_train = torch.from_numpy(self.y_train)
+            self.y_test = torch.from_numpy(self.y_test)
 
             # Training the NN
             for epoch in range(num_epochs):
                 # Zero gradients
                 self.optimiser.zero_grad()
                 # Forward pass
-                y_pred = self.model(x_train)
+                y_pred = self.model(self.x_train)
                 # Calculate loss
-                loss = self.loss_fn(y_pred, y_train)
+                loss = self.loss_fn(y_pred, self.y_train)
                 # Backward pass
                 loss.backward()
                 # Update weights
                 self.optimiser.step()
                 # Calculate metrics
                 train_acc = torch.sum(torch.argmax(
-                    y_pred, dim=1) == y_train) / y_train.shape[0]
+                    y_pred, dim=1) == self.y_train) / self.y_train.shape[0]
                 with torch.no_grad():
-                    y_pred_test = self.model(x_test)
-                    test_loss = self.loss_fn(y_pred_test, y_test)
+                    y_pred_test = self.model(self.x_test)
+                    test_loss = self.loss_fn(y_pred_test, self.y_test)
                     test_acc = torch.sum(torch.argmax(
-                        y_pred_test, dim=1) == y_test) / y_test.shape[0]
+                        y_pred_test, dim=1) == self.y_test) / self.y_test.shape[0]
                 # Log metrics
-                train_losses.append(loss.item())
-                train_accuracies.append(train_acc)
-                test_losses.append(test_loss.item())
-                test_accuracies.append(test_acc)
+                self.train_losses.append(loss.item())
+                self.train_accuracies.append(train_acc)
+                self.test_losses.append(test_loss.item())
+                self.test_accuracies.append(test_acc)
                 if epoch % 10 == 0:
                     # Print to console
                     print("Epoch {}:\tTrain loss={:.4f}  \tTrain acc={:.2f}  \tTest loss={:.4f}  \tTest acc={:.2f}".format(
                         epoch, loss.item(), train_acc*100, test_loss.item(), test_acc*100))
+
+    def export_results(self):
+        with torch.no_grad():
+            y_pred_probs_test = self.model(self.x_test).numpy()
+        y_pred_classes_test = np.argmax(y_pred_probs_test, axis=1)
+        with open('temp.txt', 'w') as f:
+            f.write(
+                f"Classification Report - \n {classification_report(self.y_test, y_pred_classes_test)} \n Confusion Matrix - \n {confusion_matrix(self.y_test, y_pred_classes_test)}")
+
+    def generate_learning_graph(self):
+        fig, (ax1, ax2) = plt.subplots(2, figsize=(12, 8), sharex=True)
+        ax1.plot(self.train_losses, color='b', label='train')
+        ax1.plot(self.test_losses, color='g', label='test')
+        ax1.set_ylabel("Loss")
+        ax1.legend()
+        ax2.plot(self.train_accuracies, color='b', label='train')
+        ax2.plot(self.test_accuracies, color='g', label='test')
+        ax2.set_ylabel("Accuracy")
+        ax2.set_xlabel("Epoch")
+        ax2.legend()
+        plt.show()
+
+    # def generate_graph(self):
+    #     activation_range = numpy.arange(-7, 7, 0.1)         # TODO fix
+    #     test_coordinates = [(self.x, self.y)
+    #                         for self.y in activation_range for self.x in activation_range]
+
+    #     x_, y_ = numpy.meshgrid(activation_range, activation_range)
+    #     plt.scatter(
+    #         x_, y_, c=['g' if x_ > 0 else 'b' for x_ in test_classifications])
+    #     plt.show()
